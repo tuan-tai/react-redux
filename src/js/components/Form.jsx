983627847a079879c72ee436beed1ad0b40080c1
Le Tuan Tai <tuantai568@gmail.com>
2019-03-20 22:53:03 +0700

    update

--- src/js/components/Form.jsx
+++ src/js/components/Form.jsx
@@ -1,38 +1,40 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, {Component} from "react";
+import {connect} from "react-redux";
 import uuidv1 from "uuid";
-import { addArticle } from "../actions/index";
+import {addArticle} from "../actions/index";
 
 function mapDispatchToProps(dispatch) {
   return {
     addArticle: article => dispatch(addArticle(article))
-  };
+  }
 }
 
 class ConnectedForm extends Component {
   constructor() {
     super();
+
     this.state = {
       title: ""
     };
+
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({ [event.target.id]: event.target.value });
+  handleChange(e) {
+    this.setState({[e.target.id]: e.target.value})
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    const { title } = this.state;
+  handleSubmit(e) {
+    e.preventDefault();
+    const {title} = this.state;
     const id = uuidv1();
-    this.props.addArticle({ title, id });
-    this.setState({ title: "" });
+    this.props.addArticle({title, id});
+    this.setState({title: ""});
   }
 
   render() {
-    const { title } = this.state;
+    const {title} = this.state;
     return (
       <form onSubmit={this.handleSubmit}>
         <div className="form-group">
